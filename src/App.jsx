2d72d823c3cbb4f15e69abe3a2015213dc06a745
David <doladepo128@gmail.com>
2023-09-17 20:37:14 +0100

    Setting up testimonial cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
+import { useState } from 'react';
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>
        <h1>Testimonials</h1>
+
+       <div className='block1'>
+         
+       </div>
+
+       <div className='block2'>
+
+       </div>
+
+       <div className='block3'>
+
+       </div>
+
+       <div className='block4'>
+
+       </div>
+
+       <div className='block5'>
+
+       </div>
       </div>
       
     </>
